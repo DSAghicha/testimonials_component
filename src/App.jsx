6682ca8e563f4ae0bbaa9e4d2952b1e4c9820aed
DSAghicha <dsaghicha211@gmail.com>
2022-04-27 09:54:46 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import './App.sass'
+import { testimonials } from './data'
+import Card from './components/card/Card'
 
 const App = () => {
     return(
-        <h1>Hello</h1>
+        <div className="app">
+            {
+                testimonials.map((testimonial, key) => {
+                    return (
+                        <Card
+                            key={ key }
+                            name={ testimonial.name }
+                            img={ testimonial.img }
+                            status={ testimonial.status }
+                            title={ testimonial.title }
+                            description={ testimonial.description }
+                        />
+                    )
+                })
+            }
+        </div>
     )
 }
 
